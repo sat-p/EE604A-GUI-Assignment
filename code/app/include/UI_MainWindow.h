ab6e9ae7abf37e8c53b95eb308379392e22977b5
Satya Prakash Panuganti <prakashsatya10@gmail.com>
2017-08-03 20:58:43 +0530

    *app    : -Added Makefile in src           -Added basic UI in the form of WC_MainWindow           -Added helper class UI_MainWindow           -Added main.cxx
    *makeinclude : Fixed error in flags.

---
+++ code/app/include/UI_MainWindow.h
@@ -0,0 +1,21 @@
+#ifndef __EE604A_GUI_MAIN_WINDOW__
+#define __EE604A_GUI_MAIN_WINDOW__
+
+#include "WC_MainWindow.h"
+
+namespace  EE604A {
+namespace GUI {
+
+class UI_MainWindow : public WC_MainWindow
+{
+public:
+    UI_MainWindow (void) = default;
+    
+private:
+    virtual void WOpen_cb (void) override;
+    virtual void WQuit_cb (void) override;
+};
+
+} } // namespaces EE604A, GUI 
+
+#endif
