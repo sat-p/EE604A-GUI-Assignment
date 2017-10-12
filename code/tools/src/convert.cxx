1312a27bfa1eb1ec683149d5b237e261adba2a2d
Satya Prakash Panuganti <prakashsatya10@gmail.com>
2017-10-12 20:55:18 +0530

    * tools/convert.cxx : Fixed bug which caused blue and red channels to be                       interchanged.

--- code/tools/src/convert.cxx
+++ code/tools/src/convert.cxx
@@ -1,13 +1,18 @@
 #include "../include/convert.h"
 
+#include <opencv2/imgproc/imgproc.hpp>
+
 /*****************************************************************************/
 
 Fl_RGB_Image* EE604A::tools::Mat_to_Fl_Image (const cv::Mat& img)
-{
-    return new Fl_RGB_Image (img.data,
-                             img.cols,
-                             img.rows,
-                             img.channels());
+{   
+    cv::Mat reversed;
+    cv::cvtColor (img, reversed, cv::COLOR_BGR2RGB);
+    
+    return new Fl_RGB_Image (reversed.data,
+                             reversed.cols,
+                             reversed.rows,
+                             reversed.channels());
 }
 
 /*****************************************************************************/
